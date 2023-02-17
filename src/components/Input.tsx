859896b673a73f5c090c0d410a3192eb5c3291c1
OmerAslani <omersatelit21@gmail.com>
2023-02-17 14:33:02 +0100

    input fixed

---
+++ src/components/Input.tsx
@@ -0,0 +1,18 @@
+
+
+
+export const Input = () =>{
+    return (
+            <div className="w-full h-10 bg-white rounded-2xl flex ">
+              <input
+                type="text"
+                placeholder="Add task"
+                className=" h-full rounded-2xl px-3 grow "/>
+              <button  className="h-full bg-primary rounded-2xl text-white capitalize px-6">
+                Click me
+              </button>
+            </div>
+            );
+    
+    
+}
