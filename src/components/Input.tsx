53f0a078037d5f2687acdb4e0274b85086356ab8
OmerAslani <omersatelit21@gmail.com>
2023-02-18 12:45:49 +0100

    console.log input value

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,14 +1,30 @@
 
+import React, { useState } from 'react';
+
+
+
 
 
 export const Input = () =>{
+    const addTask = () => {
+        console.log(InputValue);
+        setInputValue("");
+    };
+  
+
+    const [InputValue, setInputValue] = useState("");
     return (
+      
             <div className="w-full h-10 bg-white rounded-2xl flex focus-within:outline ">
               <input
                 type="text"
                 placeholder="Add task"
+                value={InputValue   }
+                onChange={(e) => setInputValue(e.target.value)}
                 className=" h-full rounded-2xl px-3 grow focus:outline-none "/>
-              <button  className="h-full bg-primary rounded-2xl text-white capitalize px-6">
+              <button  className="h-full bg-primary rounded-2xl text-white capitalize px-6"
+              onClick={addTask}
+              >
                 Click me
               </button>
             </div>
--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -5,11 +5,11 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 export const Task = () => {
     return(
         <li className='w-full h-14 flex items-center pl-4 border-b-2 border-gray-500  justify-between'>Task 1
-        <div>
-        <FontAwesomeIcon
+        <div className="">
+        <FontAwesomeIcon className="pr-2 text-xl hover:text-2xl"
           icon={faPenToSquare}     
         />
-        <FontAwesomeIcon
+        <FontAwesomeIcon className="pr-2 text-xl hover:text-2xl"
           icon={faTrash}
         />
         </div>
